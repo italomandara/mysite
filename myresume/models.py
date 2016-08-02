c9d00d21a5d1b861e67c7aab9ffc85ba100683a0
italomandara <italo.mandara@gmail.com>
2016-08-02 23:44:42 +0100

    added test template and first db

--- myresume/models.py
+++ myresume/models.py
@@ -3,3 +3,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=200)
+    person_lastname = models.CharField(max_length=200)
+    person_role = models.CharField(max_length=200)
+    person_birthday = models.DateTimeField('Birthday')
+
+
+class Skill(models.Model):
+	PRINT = 'PR'
+	DESIGN = 'DS'
+	CODING = 'CO'
+	HUMAN = 'HU'
+	SKILL_TYPES = (
+        (PRINT , 'Print'),
+        (DESIGN , 'Design'),
+        (CODING , 'Coding'),
+        (HUMAN , 'Human')
+    )
+	skill_type = models.CharField(
+    	max_length = 2,
+    	choices = SKILL_TYPES,
+    	default = CODING,
+    )
+	skill_name = models.CharField(max_length=200)
+	skill_description = models.CharField(max_length=200)
+	skill_icon = models.CharField(max_length=200)
+	skill_rating = models.IntegerField(default=0)
+	skill_category = models.CharField(max_length=200)
+
+	def get_skill_type(self):
+		return self.skill_type
--- myresume/views.py
+++ myresume/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-from django.http import HttpResponse
+from .models import Person, Skill
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+	persons_list = Person.objects.all()
+	skills_list = Skill.objects.all()
+	template = loader.get_template('main.html')
+	context = {
+		'persons_list': persons_list,
+		'skills_list': skills_list,
+    }
+	return HttpResponse(template.render(context, request))
