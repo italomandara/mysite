d919d29cb9951d28a49b174720df5e4dacc15155
italomandara <italo.mandara@gmail.com>
2016-08-06 16:19:37 +0100

    added markdown support

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'myresume.apps.MyresumeConfig',
     'sass_processor',
+    'django_markup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
