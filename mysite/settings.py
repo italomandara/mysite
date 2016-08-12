e0d48868b41d0ee2837c789e01699ff9b8a34909
italomandara <italo.mandara@gmail.com>
2016-08-12 22:28:44 +0100

    automatic debug disable in production

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,12 @@ ROOT_PATH = os.path.dirname(__file__)
 SECRET_KEY = '80s8*6n!6iijjuymo(x-+xt(26n%4x6&i2c-h*a(xiq45b5syw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ 
+if socket.gethostname() == 'itmandar.herokuapp.com':
+    DEBUG = False
+else:
+    DEBUG = True
+
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'itmandar.herokuapp.com']
 
