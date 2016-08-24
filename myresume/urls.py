7f725df3e594655284d3120df8c88a25049f60e7
italomandara <italo.mandara@gmail.com>
2016-08-25 00:11:53 +0200

    fix urls

--- myresume/urls.py
+++ myresume/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'more$', views.more, name='more'),
     url(r'icons$', views.icons, name='icons'),
     url(r'contact-form$', views.ajaxContactForm, name='contact-form'),
-    url(r'thoughts$', views.thoughts, name='thoughts'),
+    url(r'thoughts/$', views.thoughts, name='thoughts'),
     url(r'thoughts/(?P<slug>[\w-]+)/$', views.thoughtsDetail, name='thoughts-detail'),
 ]
