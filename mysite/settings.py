00b99d51908ce9bdb8aa7cf07653d5282e6463d9
italomandara <italo.mandara@gmail.com>
2016-09-17 00:21:46 +0100

    fix gzip

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,6 +156,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
