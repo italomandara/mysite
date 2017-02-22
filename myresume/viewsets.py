ace221b74de8d31edf8c8d1ac8b77c4126b0eb7d
Italo Mandara <italomandara@gmail.com>
2017-02-22 01:02:56 +0000

    permissions

--- myresume/viewsets.py
+++ myresume/viewsets.py
@@ -8,36 +8,42 @@ from .permissions import IsOwnerOrReadOnly
 # ViewSets define the view behavior.
 
 class PersonViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = Person.objects.all()
 	serializer_class = PersonSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
 	filter_fields = ('__all__')
 
 class SkillViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = Skill.objects.all()
 	serializer_class = SkillSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
 	filter_fields = ('__all__')
 
 class MyContentViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = MyContent.objects.all()
 	serializer_class = MyContentSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
 	filter_fields = ('__all__')
 
 class JobViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = Job.objects.all()
 	serializer_class = JobSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
 	filter_fields = ('__all__')
 
 class CourseViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = Course.objects.all()
 	serializer_class = CourseSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
 	filter_fields = ('__all__')
 
 class PostViewSet(viewsets.ModelViewSet):
+	http_method_names = ['get']
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
 	permissions_classes = (IsOwnerOrReadOnly,)
