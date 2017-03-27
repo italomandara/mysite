af39fc8bac726bf5d073639849aaddd7571d87c9
Italo Mandara <italomandara@gmail.com>
2017-03-27 16:47:22 +0100

    refinements

--- mysite/static/js/views.js
+++ mysite/static/js/views.js
@@ -117,66 +117,54 @@ env.addFilter('slugify', function(str) {
 var indexView = function(template, context) {
 
 	var template = 'home/ind.html';
-	var context = {
-		hostname: window.location.hostname
-	};
+	var context = {};
 
 	var settings = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/s/',
 	});
 
 	var person = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/person/?name=Italo&format=json',
 	});
 
 	var categories = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/categories/?format=json',
 	});
 
 	var skill = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/skill/?format=json',
 	});
 
 	var intro = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/mycontent/?slug=intro&format=json',
 	});
 
 	var achievements = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/mycontent/?slug=achievements&format=json',
 	});
 
 	var profile = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/mycontent/?slug=profile&format=json',
 	});
 
 	var job = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/job/?format=json',
 	});
 
 	var education = $.ajax({
-		cache: false,
 		method: 'get',
 		url: '/api/course/?format=json',
 	});
 
 	var contact = $.ajax({
-		cache: false,
 		method: 'options',
 		url: '/api/contact/?format=json',
 	});
@@ -195,12 +183,21 @@ var indexView = function(template, context) {
 			for (var idx in categories[0].post) {
 				postcategories.push({slug: idx, name: categories[0].post[idx]})
 			}
+			var getSkillCategory = function(slug){
+				return categories[0].skill[slug];
+			};
+			var getJobCategory = function(slug){
+				return categories[0].job[slug];
+			};
 			$.extend(context, {
+				hostname: window.location.hostname,
 				settings: settings[0],
 				person: person[0][0],
 				categories: categories[0],
 				skillslist: skill[0],
 				skillcategories: skillcategories,
+				getSkillCategory: getSkillCategory,
+				getJobCategory: getJobCategory,
 				skillsubcategories: [{name: 'test1'}, {name: 'test2'}],
 				intro: intro[0][0],
 				postcategories: postcategories,
