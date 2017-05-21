3bf8b849f2d10ef47a0f63c2764def107da7b77f
Italo Mandara <italomandara@gmail.com>
2017-05-21 19:18:08 +0100

    troubleshooting?

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: rm debug.log; python manage.py migrate;python manage.py dumpdata > db_live.json;manage.py dumpdata myresume > mr_live.json; python manage.py collectstatic --noinput; gunicorn mysite.wsgi
+web: rm debug.log; python manage.py migrate;python manage.py dumpdata > db_live.json;manage.py dumpdata myresume > mr_live.json; python manage.py collectstatic --clear --noinput; gunicorn mysite.wsgi
 
--- myresume/urls.py
+++ myresume/urls.py
@@ -4,17 +4,7 @@ from . import views, viewsets
 from .routers import *
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
-    # url(r'more$', views.more, name='more'),
-    # url(r'icons$', views.icons, name='icons'),
-    # url(r'contact-form$', views.ajaxContactForm, name='contact-form'),
-    # url(r'thoughts/$', views.thoughts, name='thoughts'),
-    # url(r'thoughts/posts/(?P<slug>[\w-]+)/$', views.thoughtsDetail, name='thoughts-detail'),
-    # url(r'thoughts/categories/(?P<slug>[\w-]+)/$', views.thoughtsCategory, name='thoughts-category'),
-]
-
-urlpatterns += [
-    url(r'^$', views.index_vue, name='index_ng'),
+    url(r'^$', views.index_vue, name='index_vue'),
     # url(r'api/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'api/', include(router.urls), name='api'),
     url(r'api/categories/', AllCategoryViewSet.as_view(), name='all_categories'),
